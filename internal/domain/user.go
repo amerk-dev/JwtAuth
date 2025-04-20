3eb108076797150bac8017d5de04c1b4f65b8110
v_amerk <devamerkh@gmail.com>
2025-04-20 21:24:30 +0500

    fix models

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,10 +1,8 @@
 package domain
 
 type User struct {
-	ID           int64  `json:"id"`
-	Guid         string `json:"guid"`
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	Ip           string `json:"ip"`
-	Email        string `json:"email"`
+	Id    int64  `json:"id"`
+	Guid  string `json:"guid"`
+	Ip    string `json:"ip"`
+	Email string `json:"email"`
 }
--- internal/repo/models/user.go
+++ internal/repo/models/user.go
@@ -1,10 +1,8 @@
 package models
 
 type User struct {
-	Id           int64  `gorm:"primary_key"`
-	Guid         string `json:"guid"`
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	Ip           string `json:"ip"`
-	Email        string `json:"email"`
+	Id    int64  `gorm:"primary_key"`
+	Guid  string `json:"guid" gorm:"uniqueIndex"` // UUID
+	Ip    string `json:"ip"`
+	Email string `json:"email"`
 }
